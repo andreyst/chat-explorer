ec40c15f7e3b8c24b8332dc5db171341f7a7cd00
Andrey Stolbovsky <andrey.stolbovsky@gmail.com>
2020-11-15 23:59:57 +0300

    Refactor sync chat to use chat id

--- explorer/management/commands/sync_chat.py
+++ explorer/management/commands/sync_chat.py
@@ -6,26 +6,9 @@ class Command(BaseCommand):
     help = 'Send manual command to sync chat'
 
     def add_arguments(self, parser):
-        parser.add_argument('-u', '--user-id', required=True, type=int)
-        parser.add_argument('-a', '--account-id', required=True, type=int)
-        parser.add_argument('-r', '--remote-id', required=True, type=int)
+        parser.add_argument('--id', required=True, type=int)
 
     def handle(self, *args, **options):
-        chat = Chat.objects.get(
-          user=options['user_id'],
-          account_id=options['account_id'],
-          remote_id=options['remote_id'],
-        )
+        chat = Chat.objects.get(pk=options['id'])
 
         sync_telegram_chat.delay(chat.id, chat.update_generation)
-
-        # for poll_id in options['poll_ids']:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-
-        #     poll.opened = False
-        #     poll.save()
-
-        #     self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
