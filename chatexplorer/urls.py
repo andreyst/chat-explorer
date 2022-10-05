c6b0d418d8daedc53e71fb8cf022e98224d3c566
Andrey Stolbovsky <andrey.stolbovsky@gmail.com>
2022-10-06 00:08:08 +0300

    Change url to re_path
    Why, every time I turn away, Django gets a new major version?

--- chatexplorer/urls.py
+++ chatexplorer/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('explorer.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^', include('explorer.urls')),
 ]
 
 if settings.DEBUG:
   import debug_toolbar
   urlpatterns = [
-      url(r'^__debug__/', include(debug_toolbar.urls)),
+      re_path(r'^__debug__/', include(debug_toolbar.urls)),
   ] + urlpatterns
 
--- explorer/urls.py
+++ explorer/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'explorer'
 
 urlpatterns = [
-    url(r'^login$', views.login, name='login'),
-    url(r'^logout$', views.logout, name='logout'),
-    url(r'^index$', views.index, name='index'),
-    url(r'^accounts/list$', views.list_accounts, name='list_accounts'),
-    url(r'^accounts/add$', views.add_account, name='add_account'),
-    url(r'^accounts/(?P<account_id>\d+)/telegram_sign_in/(?P<telegram_phone_hash>[a-zA-Z0-9_]+)$', views.telegram_sign_in, name='telegram_sign_in'),
-    url(r'^accounts/(?P<account_id>\d+)/remote_chats/list$', views.list_remote_chats, name='list_remote_chats'),
-    url(r'^accounts/(?P<account_id>\d+)/remote_chats/add/(?P<remote_id>\d+)_(?P<remote_type>\d+)$', views.import_remote_chat, name='import_remote_chat'),
-    url(r'^chats/(?P<chat_id>\d+)/explore$', views.explore_chat, name='explore_chat'),
-    url(r'^chats/(?P<chat_id>\d+)/change_author_role$', views.change_author_role, name='change_author_role'),
-    url(r'^$', views.index, name='index'),
+    re_path(r'^login$', views.login, name='login'),
+    re_path(r'^logout$', views.logout, name='logout'),
+    re_path(r'^index$', views.index, name='index'),
+    re_path(r'^accounts/list$', views.list_accounts, name='list_accounts'),
+    re_path(r'^accounts/add$', views.add_account, name='add_account'),
+    re_path(r'^accounts/(?P<account_id>\d+)/telegram_sign_in/(?P<telegram_phone_hash>[a-zA-Z0-9_]+)$', views.telegram_sign_in, name='telegram_sign_in'),
+    re_path(r'^accounts/(?P<account_id>\d+)/remote_chats/list$', views.list_remote_chats, name='list_remote_chats'),
+    re_path(r'^accounts/(?P<account_id>\d+)/remote_chats/add/(?P<remote_id>\d+)_(?P<remote_type>\d+)$', views.import_remote_chat, name='import_remote_chat'),
+    re_path(r'^chats/(?P<chat_id>\d+)/explore$', views.explore_chat, name='explore_chat'),
+    re_path(r'^chats/(?P<chat_id>\d+)/change_author_role$', views.change_author_role, name='change_author_role'),
+    re_path(r'^$', views.index, name='index'),
 ]
